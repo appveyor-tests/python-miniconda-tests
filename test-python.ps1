23453a53d3c4467999e0244cb33a9fe6f09e82b3
Feodor Fitsner <feodor@appveyor.com>
2018-07-10 17:28:31 -0700

    Check Python 3.7

--- test-python.ps1
+++ test-python.ps1
@@ -2,6 +2,13 @@ function CheckPython($path) {
     if (-not (Test-Path "$path\python.exe")) { throw "python.exe is missing in $path"; }
     elseif (-not (Test-Path "$path\Scripts\pip.exe")) { throw "pip.exe is missing in $path"; }
     else { Write-Host "$path is OK" -ForegroundColor Green; }
+    $r = (cmd /c "$path\python.exe" --version 2>&1)
+    $r.Exception
+    if (-not $r.Exception) {
+        cmd /c "$path\python.exe" --version
+    }
+    (cmd /c "$path\Scripts\pip.exe" --version 2>&1)
+    (cmd /c "$path\Scripts\virtualenv.exe" --version 2>&1)
 }
 
 CheckPython 'C:\Python26'
@@ -16,3 +23,5 @@ CheckPython 'C:\Python35'
 CheckPython 'C:\Python35-x64'
 CheckPython 'C:\Python36'
 CheckPython 'C:\Python36-x64'
+CheckPython 'C:\Python37'
+CheckPython 'C:\Python37-x64'
